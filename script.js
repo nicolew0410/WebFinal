34dcb552017fcab1de4a5d131666c7c60ad60557
Nicole Wong <Nicoletheunicorn@Nicoles-MacBook-Pro-2.local>
2023-05-02 06:46:21 -0400

    final

---
+++ script.js
@@ -0,0 +1,15 @@
+function openPopup() {
+	document.getElementById("popup").style.display = "block";
+}
+function openPopup2() {
+	document.getElementById("popup2").style.display = "block";
+}
+function openPopup3() {
+	document.getElementById("popup3").style.display = "block";
+}
+function openPopup4() {
+	document.getElementById("popup4").style.display = "block";
+}
+function openPopup5() {
+	document.getElementById("popup5").style.display = "block";
+}
